aa36d392e6f59a11799138a68a0252a61bfe44b0
sam <snorman19@comcast.net>
2022-05-26 10:35:49 -0400

    reading docker compose files

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import docker
 import yaml
+import os
 client = docker.from_env()
+cwd = os.getcwd()
 
-with open("npm.yaml", "r") as stream:
-    try:
-        print(yaml.safe_load(stream))
-    except yaml.YAMLError as exc:
-        print(exc)
+manifests = []
+for filename in os.listdir(cwd + "/stacks/manifests/"):
+    if filename.endswith(".yml") or filename.endswith(".yaml"):
+        with open( f"{cwd}/stacks/manifests/{filename}", "r") as stream:
+            manifest = ""
+            try:
+                manifest = yaml.safe_load(stream)
+            except yaml.YAMLError as exc:
+                print(exc)
+        manifests.append(manifest)
 
+print(manifests)
 #client.containers.run("hello-world")
