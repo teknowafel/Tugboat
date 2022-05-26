24bab01d29220f4f2cf349cfd2fef5d09eb5436e
teknowafel <samnorman1572@gmail.com>
2022-05-25 20:57:23 -0400

    h

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import docker
+import yaml
 client = docker.from_env()
 
-client.containers.run("hello-world")
+with open("npm.yaml", "r") as stream:
+    try:
+        print(yaml.safe_load(stream))
+    except yaml.YAMLError as exc:
+        print(exc)
+
+#client.containers.run("hello-world")
