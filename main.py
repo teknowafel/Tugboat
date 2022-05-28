8c0a74d961930b53e001d8e9ad9a6b03ddf81f8c
teknowafel <samnorman1572@gmail.com>
2022-05-28 14:00:30 -0400

    starts scheduler, need to keep app running

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from apscheduler.schedulers.background import BackgroundScheduler
 import yaml
 import os
 from lib.compose import check, up, update
+import lib.schedule
 cwd = os.getcwd()
 
 def load_stacks():
@@ -39,9 +41,22 @@ def initialize_stacks(stacks):
             else:
                 print(f"Error starting stack {stack['appname']}")
 
+        print(f"Scheduling updates for stack {stack['appname']}")
+        scheduler = BackgroundScheduler()
+        try:
+            if stack["updates"]["type"] == "interval":
+                lib.schedule.schedule_interval(stack, scheduler, stack["updates"]["interval"])
+                print(f"{stack['appname']} scheduled to update every {stack['updates']['interval']} seconds")
+            elif stack["updates"]["type"] == "cron":
+                lib.schedule.schedule_cron(stack, scheduler, stack["updates"]["expression"])
+                print(f"{stack['appname']} scheduled to update at {stack['updates']['expression']} seconds")
+        except:
+            print(f"Malformed or no schedule configuration for stack {stack['appname']}. Updates will not be scheduled.")
+    
+    scheduler.start()
+    print("Scheduler has been started")
+
 
 if __name__ == "__main__":
     stacks = load_stacks()
     initialize_stacks(stacks)
-        
-
