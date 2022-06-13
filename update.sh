33bbd59f71c776341f808389a8b7640a082f1f72
teknowafel <samnorman1572@gmail.com>
2022-06-13 17:36:33 -0400

    comments, stop, and all keyword

--- update.sh
+++ update.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+
+# Install depedencies for debian
 apt-get update && apt-get install -y \
 rsync \
 git
 
+# Remove old stuff
 rm -r /opt/tugboat/
 rm /usr/bin/tugboat
+
+# Create the log file and give permissions to write to it
 touch /var/log/tugboat && chmod 666 /var/log/tugboat
+
+# Clone tugboat source into /opt/tugboat and make a symlink so it can be executed conveniently
 git clone https://github.com/teknowafel/Tugboat /opt/tugboat
 ln -s /opt/tugboat/tugboat /usr/bin/tugboat
