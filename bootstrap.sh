4840f2d0beaccd6b12f61f890a0cc98c6198f26c
teknowafel <samnorman1572@gmail.com>
2022-06-13 17:00:51 -0400

    fixes

--- bootstrap.sh
+++
@@ -1,5 +0,0 @@
-echo "Tugboat has been initialized, scheduling jobs"
-crontab /config
-cron -f &
-echo "Jobs have been scheduled, you are now viewing the log"
-tail -f /var/log/tugboat
--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-apt-get update && apt-get install -y \
-rsync \
-git
-
-git clone https://github.com/teknowafel/Tugboat /opt/tugboat
-ln -s /opt/tugboat /usr/bin/tugboat
