eca34c620a5a7f65773a8138812bc1e03fc90083
teknowafel <samnorman1572@gmail.com>
2022-06-13 17:26:02 -0400

    fix permissions issue

--- update.sh
+++ update.sh
@@ -5,5 +5,6 @@ git
 
 rm -r /opt/tugboat/
 rm /usr/bin/tugboat
+touch /var/log/tugboat && chmod 666 /var/log/tugboat
 git clone https://github.com/teknowafel/Tugboat /opt/tugboat
 ln -s /opt/tugboat/tugboat /usr/bin/tugboat
