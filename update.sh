22fe40d29946792ab8e9f2e4ec3688a3e2cde714
teknowafel <samnorman1572@gmail.com>
2022-06-18 11:07:05 -0400

    use /var/opt/tugboat as /var/tmp is cleared

--- update.sh
+++ update.sh
@@ -13,6 +13,9 @@ touch /var/log/tugboat && chmod 666 /var/log/tugboat
 
 mkdir -p /opt/tugboat
 
+mkdir -p /var/opt/tugboat/
+chmod -R 666 /var/opt/tugboat/
+
 # Clone tugboat source into /opt/tugboat and make a symlink so it can be executed conveniently
 curl https://raw.githubusercontent.com/teknowafel/Tugboat/main/tugboat > /opt/tugboat/tugboat
 chmod +x /opt/tugboat/tugboat
